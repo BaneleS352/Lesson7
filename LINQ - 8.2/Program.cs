6853be67a4e0d8c682cc88a4d308e997dcb39e93
BaneleS352 <BaneleS@betsoftware.com>
2026-03-24 11:25:34 +0200

    8.2
    8.2

---
+++ LINQ - 8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mini Exercise 2: Top 3 Numbers");
+        Console.WriteLine("------------------------------");
+
+        Mini_TopThree();
+
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void Mini_TopThree()
+    {
+        // TODO:
+        // 1. Create the list of numbers.
+        var numbers = new List<int>{ 10, 12, 65, 23, 5, 9, 78, 23 };
+        
+        // 2. Print the original list.
+        Console.WriteLine($"Original list: ");
+        Console.WriteLine(string.Join(",", numbers));
+        // 3. Use OrderByDescending and Take(3).
+
+        // 4. Convert the result to a list.
+        var order = numbers.OrderByDescending(n => n).Take(3).ToList();
+        // 5. Print the top 3 numbers.
+        Console.WriteLine($"Top 3 Descending order: ");
+        Console.WriteLine(string.Join(",", order));
+    }
+}
