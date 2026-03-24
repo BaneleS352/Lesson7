497b26186c3d5ba21618e73451bed817f22cc797
BaneleS352 <BaneleS@betsoftware.com>
2026-03-24 11:44:49 +0200

    8.4
    8.4

---
+++ LINQ - 8.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+// TODO:
+// 1. Create a list of integers.
+var num = new List<int> {1,2,3,4,5,6,7,8,9 };
+// 2. Group them by "Even" and "Odd" with GroupBy.
+var grouped = num.GroupBy(n => n % 2 == 0 ? "Even" : "Odd");
+// 3. Loop through each group.
+// 4. Print the group label and the numbers in that group.
+// TODO: print each group here
+foreach (var group in grouped)
+{
+    Console.WriteLine(group.Key + ": " + string.Join(", ", group));
+}
